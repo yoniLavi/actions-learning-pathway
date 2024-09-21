5f4d13b0c9ca596d34e8b3f55e7a8978c10bdaaa
Yoni Lavi <yoni@lavi.fm>
2024-09-21 08:27:17 +0000

    Fixed minor linter warning

--- pages/index.js
+++ pages/index.js
@@ -16,8 +16,8 @@ export default function Home() {
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
 
-       <h1>Congratulations!</h1>
-  <p>You've completed the essentials of automated application deployment with GitHub Actions! 🥳</p>
+        <h1>Congratulations!</h1>
+        <p>You&apos;ve completed the essentials of automated application deployment with GitHub Actions! 🥳</p>
 
       </main>
     </>
